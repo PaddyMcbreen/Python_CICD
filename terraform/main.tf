61ea168dd8a1600f212cfd9582ac01f5604444f4
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-07-16 16:19:31 +0100

    Codestar connection resource created

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,8 @@
 #============================================================================#
-# AWS CodePipeline
+# AWS CodePipeline - CodeStar Connection
 #============================================================================#
 
+resource "aws_codestarconnections_connection" "codestar_connection" {
+  name          = "GitHub_Connection"
+  provider_type = "GitHub"
+}
