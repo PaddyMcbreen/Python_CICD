b373b6eb4a92b3fdf349aba969db4b68a09c0af1
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-07-16 16:37:50 +0100

    Creates template/shell for aws codepipeline (terraform docs)

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,11 @@ resource "aws_codestarconnections_connection" "codestar_connection" {
   name          = "GitHub_Connect"
   provider_type = "GitHub"
 }
+
+#============================================================================#
+# AWS CodePipeline - IaC
+#============================================================================#
+
+resource "aws_codepipeline" "codepipeline" {
+  name     = "tf-test-pipeline"
+}
