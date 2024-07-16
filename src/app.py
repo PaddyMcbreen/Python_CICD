da4f82a7d5f070a3761efa67c1afae3a0f61f3c4
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-07-16 16:09:46 +0100

    Reformatting file structure - creating dirs and moving files

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import sqlite3
 import random
 import subprocess
-from database import init_db, save_score
+from src.database import init_db, save_score
 
 # Give scripts required permissions:
 subprocess.run(["chmod +x ./scripts/permissions.sh"], shell=True)
