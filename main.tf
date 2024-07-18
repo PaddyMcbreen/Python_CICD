cbb6a19bd1beb5b77ac35735d910880fddd75930
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-07-18 10:55:24 +0100

    S3 bucket debugging changes - new iam role for artifact store bucket created - tfstate updated with latest state

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "aws_codestarconnections_connection" "codestar_connection" {
 # AWS CodePipeline - S3 Buckets
 #============================================================================#
 
-resource "aws_s3_bucket" "codepipeline_storage_bucket" {
-  bucket = "codepipeline-bucket"
+resource "aws_s3_bucket" "codepipelines_artifacts_bucket" {
+  bucket = "codepipeline-artifact-bucket"
 }
 
 #============================================================================#
@@ -24,8 +24,8 @@ resource "aws_codepipeline" "codepipeline" {
   role_arn = aws_iam_role.codepipeline_role.arn
 
   artifact_store {
-    location = aws_s3_bucket.codepipeline_storage_bucket.bucket
-    type     = "S3"
+  location = aws_s3_bucket.codepipelines_artifacts_bucket.bucket
+  type     = "S3"
   }
 
   stage {
@@ -66,15 +66,6 @@ resource "aws_codepipeline" "codepipeline" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_pab" {
-  bucket = aws_s3_bucket.codepipeline_storage_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
@@ -93,6 +84,34 @@ resource "aws_iam_role" "codepipeline_role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+resource "aws_iam_role_policy" "artifact_store_bucket_policy" {
+  name   = "ArtifactStoreBucket"
+  role   = aws_iam_role.codepipeline_role.name
+
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:GetObjectVersion",
+          "s3:PutObjectAcl",
+          "s3:GetBucketAcl",
+          "s3:GetBucketLocation",
+          "s3:ListBucket",
+          "s3:DeleteObject",
+          "s3:DeleteBucket"
+        ],
+        Resource = [
+          "${aws_s3_bucket.codepipelines_artifacts_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
 data "aws_iam_policy_document" "codepipeline_policy" {
   statement {
     effect = "Allow"
@@ -106,8 +125,8 @@ data "aws_iam_policy_document" "codepipeline_policy" {
     ]
 
     resources = [
-      aws_s3_bucket.codepipeline_storage_bucket.arn,
-      "${aws_s3_bucket.codepipeline_storage_bucket.arn}/*"
+      aws_s3_bucket.codepipelines_artifacts_bucket.arn,
+      "${aws_s3_bucket.codepipelines_artifacts_bucket.arn}/*"
     ]
   }
 
