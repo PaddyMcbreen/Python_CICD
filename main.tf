661dd8cd0157f94daa433bbc3752117b0e1835f4
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-05 11:57:53 +0100

    changes service role to IAM codebuild role in codebuild config

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "aws_codepipeline" "codepipeline" {
 #============================================================================#
 resource "aws_codebuild_project" "codebuild_project" {
   name           = "test-codebuild-project"
-  service_role = aws_iam_role.codepipeline_role.arn
+  service_role = aws_iam_role.codebuild_role.arn
 
   artifacts {
     type = "CODEPIPELINE"
@@ -145,7 +145,6 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
       {
         Effect = "Allow"
         Action = [
-          "*",
           "codebuild:BatchGetBuilds",
           "codebuild:StartBuild",
           "codebuild:StopBuild"
