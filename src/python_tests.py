97cfda56f02c822c5fb1f0693f6582dcdc5e6990
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-05 16:00:55 +0100

    Unit test created to check variable attempts in play_game function initialises at 0

--- src/python_tests.py
+++ src/python_tests.py
@@ -1,10 +1,19 @@
 import unittest
+from app import play_game
 
-class pythonTesting(unittest.TestCase):
+class testFunc(unittest.TestCase):
     def test_func_1(self):
         pass
 
+class pythonTests(unittest.TestCase):
+    def test_zeroAttempts(self):
+        # Arrange & Act
+        attempts = play_game(attempts)
+        # Assert
+        self.assertEqual(attempts, 0, "The variable 'attempts' should start at 0.")
 
 
+
+# ---------------------------
 if __name__ == "__main__":
     unittest.main()
