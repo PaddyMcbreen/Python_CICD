cda7ed10fa9d8d0582b45d1cb77023d458a9a9f1
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-05 14:57:14 +0100

    Creates unneccessary fill in tests to show working pytest tool used within buildspec for pipeline

--- python_tests.py
+++ python_tests.py
@@ -1,11 +1,11 @@
 # Add required imports when creating further tests.
+import python_tests
 
 # Unneccesarry fill in tests to show pytest working:
 class fillInTests():
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+ def test_upper():
+    assert 'foo'.upper() == 'FOO'
+    
+ def test_isupper():
+    assert 'FOO'.isupper()
+    assert not 'Foo'.isupper()
