182e7a10657b84de31b046efcae6c150465567b9
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-05 08:55:29 +0100

    Changes name of sqlite database to scores.db - matches databse.py name

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS scores;
+DROP TABLE IF EXISTS scores.db;
 
-CREATE TABLE scores (
+CREATE TABLE scores.db (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     attempts INTEGER NOT NULL
 );
