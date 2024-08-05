bed8deb7bdd7279af672b3ad7f79c597eff376a3
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-05 14:25:43 +0100

    uses pytest command in buildspec to run tests on test file. Test file created (python_tests.py) no tests in use atm

---
+++ python_tests.py
@@ -0,0 +1,8 @@
+# There are currently no tests that have been created or are being used, this file is just a place keeper for if tests are to be implemented in the future.
+
+# Import any tools you may need (e.g. pytest).
+# Create any tests for the code.
+
+# Adjust the buildspec.yml file to use this file for testing with the pytest tool (already done)
+
+print("The buildspec file has successfully used pytest on the python_tests.py file. No tests are currently in use.")
