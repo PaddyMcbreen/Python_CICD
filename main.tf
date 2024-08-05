37f6b5b58ea534166eda0e05d740485ea5997232
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-05 11:43:23 +0100

    codebuild configuration for pipeline created

--- main.tf
+++ main.tf
@@ -67,6 +67,35 @@ resource "aws_codepipeline" "codepipeline" {
 }
 
 #============================================================================#
+# AWS CodeBuild Config
+#============================================================================#
+resource "aws_codebuild_project" "codebuild_project" {
+  name           = "test-codebuild-project"
+  service_role = aws_iam_role.codepipeline_role.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/mitchellh/packer.git"
+    git_clone_depth = 1
+  }
+
+  tags = {
+    Environment = "test_codebuild"
+  }
+}
+
+#============================================================================#
 # AWS IAM Roles and Policies
 #============================================================================#
 
