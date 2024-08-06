ca32407721e0965f8ab1001f3281b18b654bc2e7
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-06 11:54:24 +0100

    fixing comments so that they show in terminal when tests are ran

--- src/python_tests.py
+++ src/python_tests.py
@@ -12,20 +12,20 @@ class userInputTests(unittest.TestCase):
     @patch('builtins.input', return_value = int(50))
     def test_guessIsNum(self, mock_input):
         # Arrange & Act
-        "Checks that the user input will work with a random integer"
+        print("Checks that the user input will work with a random integer")
         guess = play_game()        
         # Assert
         self.assertEqual(type(guess), int)
-        "Test Complete - Passed"
+        print("Test Complete - Passed")
 
     @patch('builtins.input', return_value = int("50"))
     def test_guessIsNum2(self, mock_input):
         # Arrange & Act
-        "Checks that the user input with convert a numbered string to an integer"
+        print("Checks that the user input with convert a numbered string to an integer")
         guess = play_game()        
         # Assert
         self.assertEqual(type(guess), int)
-        "Test Complete - Passed"
+        print("Test Complete - Passed")
 
 
 class expectedFailureTests(unittest.TestCase):
@@ -33,11 +33,11 @@ class expectedFailureTests(unittest.TestCase):
     # @unittest.expectedFailure
     def test_UserInputStr(self, mock_input):
         # Arrange & Act
-        "Checks that the user input will not allow strings that cannot convert to a number"
+        print("Checks that the user input will not allow strings that cannot convert to a number")
         guess = play_game()        
         # Assert
         self.assertEqual(type(guess), str)
-        "Test Complete - Passed"
+        print("Test Complete - Passed")
 
 
 
