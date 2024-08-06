0ab63cb1c65e09f8627fe4f990bbd7011c428036
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-06 11:50:56 +0100

    Adds comments to tests to display what they do and when they are complete

--- src/python_tests.py
+++ src/python_tests.py
@@ -4,6 +4,7 @@ from app import play_game
 
 class testFunc(unittest.TestCase):
     def test_func_1(self):
+        "This is a test function which provides no use"
         pass
 
 
@@ -11,16 +12,20 @@ class userInputTests(unittest.TestCase):
     @patch('builtins.input', return_value = int(50))
     def test_guessIsNum(self, mock_input):
         # Arrange & Act
+        "Checks that the user input will work with a random integer"
         guess = play_game()        
         # Assert
         self.assertEqual(type(guess), int)
+        "Test Complete - Passed"
 
     @patch('builtins.input', return_value = int("50"))
     def test_guessIsNum2(self, mock_input):
         # Arrange & Act
+        "Checks that the user input with convert a numbered string to an integer"
         guess = play_game()        
         # Assert
         self.assertEqual(type(guess), int)
+        "Test Complete - Passed"
 
 
 class expectedFailureTests(unittest.TestCase):
@@ -28,9 +33,11 @@ class expectedFailureTests(unittest.TestCase):
     # @unittest.expectedFailure
     def test_UserInputStr(self, mock_input):
         # Arrange & Act
+        "Checks that the user input will not allow strings that cannot convert to a number"
         guess = play_game()        
         # Assert
         self.assertEqual(type(guess), str)
+        "Test Complete - Passed"
 
 
 
