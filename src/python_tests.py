28978693df496c63ee9256573e8dd7028dd3a282
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-06 11:57:32 +0100

    Adds spacing and formatting to testing to display better in terminal

--- src/python_tests.py
+++ src/python_tests.py
@@ -12,20 +12,24 @@ class userInputTests(unittest.TestCase):
     @patch('builtins.input', return_value = int(50))
     def test_guessIsNum(self, mock_input):
         # Arrange & Act
+        print("------------------------------------------------------------")
+        print("                                                            ")
         print("Checks that the user input will work with a random integer")
-        guess = play_game()        
+        guess = play_game()
+        print("Test Complete - Passed")        
         # Assert
         self.assertEqual(type(guess), int)
-        print("Test Complete - Passed")
 
     @patch('builtins.input', return_value = int("50"))
     def test_guessIsNum2(self, mock_input):
         # Arrange & Act
+        print("------------------------------------------------------------")
+        print("                                                            ")
         print("Checks that the user input with convert a numbered string to an integer")
-        guess = play_game()        
+        guess = play_game() 
+        print("Test Complete - Passed")       
         # Assert
         self.assertEqual(type(guess), int)
-        print("Test Complete - Passed")
 
 
 class expectedFailureTests(unittest.TestCase):
@@ -33,12 +37,13 @@ class expectedFailureTests(unittest.TestCase):
     # @unittest.expectedFailure
     def test_UserInputStr(self, mock_input):
         # Arrange & Act
+        print("------------------------------------------------------------")
+        print("                                                            ")
         print("Checks that the user input will not allow strings that cannot convert to a number")
-        guess = play_game()        
+        guess = play_game()  
+        print("Test Complete - Passed")      
         # Assert
         self.assertEqual(type(guess), str)
-        print("Test Complete - Passed")
-
 
 
 # ---------------------------
