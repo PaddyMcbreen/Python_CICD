10a158f8421ea74cdc8d9874c8ca60c1f2448006
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-13 10:59:54 +0100

    uncommenting final test to have all test running - all should pass

--- src/python_tests.py
+++ src/python_tests.py
@@ -37,23 +37,23 @@ class pythonAppTests(unittest.TestCase):
 
 
     # Tests that the game can be replayed:
-    # @patch('builtins.input', side_effect=[23, 86, "y", 45, "n"])
-    # @patch('random.randint', side_effect=[86, 45])
-    # def test_incorrectGuess (self, mock_randint, mock_input):
-    #     # Arrange & Act
-    #     print("------------------------------------------------------------")
-    #     print("                                                            ")
-    #     print("Checks that the game is able to be replayed")
-    #     guess = play_game()
-    #     guess_2 = play_game()
-    #     print("Test Complete - Passed")   
-    #     print("                                                            ")     
-    #     # Assert
-    #     mock_input.assert_called()
-    #     mock_randint.assert_called_with(1, 100)
+    @patch('builtins.input', side_effect=[23, 86, "y", 45, "n"])
+    @patch('random.randint', side_effect=[86, 45])
+    def test_incorrectGuess (self, mock_randint, mock_input):
+        # Arrange & Act
+        print("------------------------------------------------------------")
+        print("                                                            ")
+        print("Checks that the game is able to be replayed")
+        guess = play_game()
+        guess_2 = play_game()
+        print("Test Complete - Passed")   
+        print("                                                            ")     
+        # Assert
+        mock_input.assert_called()
+        mock_randint.assert_called_with(1, 100)
 
-    #     mock_input.assert_called()
-    #     mock_randint.assert_called_with(1, 100)
+        mock_input.assert_called()
+        mock_randint.assert_called_with(1, 100)
 
 #---------------------------
 if __name__ == "__main__":
