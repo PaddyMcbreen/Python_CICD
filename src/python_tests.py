1a22a895a43774d9f9e2ecd10eb9295efbb65624
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-13 11:03:28 +0100

    re-ordering tests

--- src/python_tests.py
+++ src/python_tests.py
@@ -20,22 +20,6 @@ class pythonAppTests(unittest.TestCase):
         mock_randint.assert_called_with(1, 100)
 
 
-    # Tests the user input when taking a guess:
-    @patch('builtins.input', side_effect=[35, 40, "n"])
-    @patch('random.randint', return_value=40)
-    def test_incorrectGuess (self, mock_randint, mock_input):
-        # Arrange & Act
-        print("------------------------------------------------------------")
-        print("                                                            ")
-        print("Checks that the game respondes correcly to an incorrect guess")
-        guess = play_game()
-        print("Test Complete - Passed")   
-        print("                                                            ")     
-        # Assert
-        mock_input.assert_called()
-        mock_randint.assert_called_with(1, 100)
-
-
     # Tests that the game can be replayed:
     @patch('builtins.input', side_effect=[23, 86, "y", 45, "n"])
     @patch('random.randint', side_effect=[86, 45])
@@ -52,6 +36,22 @@ class pythonAppTests(unittest.TestCase):
         mock_input.assert_called()
         mock_randint.assert_called_with(1, 100)
 
+        mock_input.assert_called()
+        mock_randint.assert_called_with(1, 100)
+
+
+    # Tests the user input when taking a guess:
+    @patch('builtins.input', side_effect=[35, 40, "n"])
+    @patch('random.randint', return_value=40)
+    def test_incorrectGuess (self, mock_randint, mock_input):
+        # Arrange & Act
+        print("------------------------------------------------------------")
+        print("                                                            ")
+        print("Checks that the game respondes correcly to an incorrect guess")
+        guess = play_game()
+        print("Test Complete - Passed")   
+        print("                                                            ")     
+        # Assert
         mock_input.assert_called()
         mock_randint.assert_called_with(1, 100)
 
