622cb57be3d479ae2392577898d690f5669cd089
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-13 09:12:02 +0100

    debugging test to check if game responds correctly to incorrect guess

--- src/python_tests.py
+++ src/python_tests.py
@@ -27,7 +27,8 @@ class pythonAppTests(unittest.TestCase):
        # self.assertEqual(type(guess), int)
 
     # Tests the user input when taking a guess:
-    @patch('builtins.input', return_value = int(35, 40))
+    @patch('builtins.input', return_value = int(35))
+    @patch('builtins.input', return_value = int(40))
     @patch('random.randint', return_value=40)
     def test_incorrectGuess (self, mock_randint, mock_input):
         # Arrange & Act
