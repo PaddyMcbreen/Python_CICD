4a85628b88ea6e3eeee3bbb03a05203e6b3c5caa
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-13 10:48:05 +0100

    commenting out unit tests to debug non-showing test

--- src/python_tests.py
+++ src/python_tests.py
@@ -5,19 +5,20 @@ from app import play_game
 class pythonAppTests(unittest.TestCase):
 
     # Tests the user input when taking a guess:
-    @patch('builtins.input', return_value = int(50))
-    @patch('random.randint', return_value=50)
-    def test_correctGuess (self, mock_randint, mock_input):
-        # Arrange & Act
-        print("------------------------------------------------------------")
-        print("                                                            ")
-        print("Checks that the game passes when the num is correctly guessed")
-        guess = play_game()
-        print("Test Complete - Passed")   
-        print("                                                            ")     
-        # Assert
-        mock_input.assert_called()
-        mock_randint.assert_called_with(1, 100)
+    # @patch('builtins.input', return_value = int(50))
+    # @patch('random.randint', return_value=50)
+    # def test_correctGuess (self, mock_randint, mock_input):
+    #     # Arrange & Act
+    #     print("------------------------------------------------------------")
+    #     print("                                                            ")
+    #     print("Checks that the game passes when the num is correctly guessed")
+    #     guess = play_game()
+    #     print("Test Complete - Passed")   
+    #     print("                                                            ")     
+    #     # Assert
+    #     mock_input.assert_called()
+    #     mock_randint.assert_called_with(1, 100)
+
 
     # Tests the user input when taking a guess:
     @patch('builtins.input', side_effect=[35, 40, "n"])
@@ -34,24 +35,25 @@ class pythonAppTests(unittest.TestCase):
         mock_input.assert_called()
         mock_randint.assert_called_with(1, 100)
 
-    # Tests that the game can be replayed:
-    @patch('builtins.input', side_effect=[23, 86, "y", 45, "n"])
-    @patch('random.randint', side_effect=[86, 45])
-    def test_incorrectGuess (self, mock_randint, mock_input):
-        # Arrange & Act
-        print("------------------------------------------------------------")
-        print("                                                            ")
-        print("Checks that the game is able to be replayed")
-        guess = play_game()
-        guess_2 = play_game()
-        print("Test Complete - Passed")   
-        print("                                                            ")     
-        # Assert
-        mock_input.assert_called()
-        mock_randint.assert_called_with(1, 100)
 
-        mock_input.assert_called()
-        mock_randint.assert_called_with(1, 100)
+    # Tests that the game can be replayed:
+    # @patch('builtins.input', side_effect=[23, 86, "y", 45, "n"])
+    # @patch('random.randint', side_effect=[86, 45])
+    # def test_incorrectGuess (self, mock_randint, mock_input):
+    #     # Arrange & Act
+    #     print("------------------------------------------------------------")
+    #     print("                                                            ")
+    #     print("Checks that the game is able to be replayed")
+    #     guess = play_game()
+    #     guess_2 = play_game()
+    #     print("Test Complete - Passed")   
+    #     print("                                                            ")     
+    #     # Assert
+    #     mock_input.assert_called()
+    #     mock_randint.assert_called_with(1, 100)
+
+    #     mock_input.assert_called()
+    #     mock_randint.assert_called_with(1, 100)
 
 #---------------------------
 if __name__ == "__main__":
